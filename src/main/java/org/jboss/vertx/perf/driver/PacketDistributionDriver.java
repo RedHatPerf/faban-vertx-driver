22b23886a85a12b55c455973d7dc36dbce127ed5
Jeremy Whiting <jwhiting@redhat.com>
2017-04-12 12:37:52 +0100

    Added content type again.

--- src/main/java/org/jboss/vertx/perf/driver/PacketDistributionDriver.java
+++ src/main/java/org/jboss/vertx/perf/driver/PacketDistributionDriver.java
@@ -370,6 +370,7 @@ public class PacketDistributionDriver implements Serializable {
             throw new FatalException("Exception using the xpath to get the [content-size-in-bytes] value.", xpathe);
          }
          headers.put("Keep-Alive", "true");
+	 headers.put("Content-Type", "application/json");
       } catch (Throwable e) {
          throw new FatalException("Fatal error during configure.", e);
       } finally {
